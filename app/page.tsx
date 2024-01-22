a2316179669e3a10d7165fd461e6df88d1f10751
Chirag Jain <cjain8060@gmail.com>
2024-01-22 22:37:02 +0530

    Dummy Push

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <div className="text-red-500">Hello There Testing Tailwind</div>
+      <div className="text-red-500">Dummy</div>
       <Button>hello</Button>
     </div>
   );
