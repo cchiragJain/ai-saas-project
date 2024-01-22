3b45c5868a7cfad2560251ff7014f2368da9165f
Chirag Jain <cjain8060@gmail.com>
2024-01-23 00:55:52 +0530

    Added sidebar for mobile view using shad cn sheet

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
+import Sidebar from "./sidebar";
+
+const MobileSidebar = () => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileSidebar;
